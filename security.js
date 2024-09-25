3114984095c0601e4104bd8455a7ff0068a8ea70
Carlo <mangxander22@gmail.com>
2024-09-25 21:59:29 +0800

    Update security.js

--- security.js
+++ security.js
@@ -1,9 +1,23 @@
-// Disable right-click, F12, Ctrl+Shift+I, Ctrl+U
+// Disable right-click
 document.addEventListener('contextmenu', (e) => e.preventDefault());
 
+// Disable F12 key
 document.onkeydown = function (e) {
-    if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) || 
-        (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+};
+
+// Disable Ctrl+Shift+I
+document.onkeydown = function (e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+};
+
+// Disable Ctrl+U
+document.onkeydown = function (e) {
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
         return false;
     }
 };
